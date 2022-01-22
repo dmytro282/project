8ce2f41cc4306b95d14a3d93feea37986cdc6f6f
Dmytro <dmytrol777@gmail.com>
2022-01-22 13:31:55 +0200

    1 practice

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
- 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-
-const category = 'toys';
-
-console.log(`https://someurl.com/${category}/5`);
-
-const user = "Dmytro";
-
-alert(`Привет,${user}`);
-
-
+console.log(personalMovieDB);
